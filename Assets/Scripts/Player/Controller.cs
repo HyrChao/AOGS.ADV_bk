899f36d8b8c6cdcc1f05c65efd475bf349fd097b
董 超 <hyrchao@outlook.com>
2018-03-20 18:45:42 +0800

    re-write controller

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -5,38 +5,92 @@ using UnityEngine;
 public class Controller : MonoBehaviour {
 
     public Player player;
-    private AnimeManager am;
-    private float moveSpeed;
-    private float runSpeed;
+    //private AnimeManager am;
+    public GameObject charaMesh;
 
+    private float moveAxis = 0f;
+    private float moveSpeed = 8.0f;
+    Quaternion frontRotation = Quaternion.Euler(0f, 115f, 0f);    //欧拉角到四元数变换  def:115&-115
+    Quaternion backRotation = Quaternion.Euler(0f, 245f, 0f);
+    bool grounded = false;
+    bool facingRight = false;
 
+    private float runSpeed = 0f;
     public float jumpForce = 50000f;
 
     private Rigidbody rb; // Reference to rigidbody
     private LayerMask environmentLayerMask; // Layer for raycast to hit
-    private float lastDistance; // The last distance between the player and the environment (directly down)
-                                // Use this for initialization
+    private float lastDistance; // The last distance between the player and the environment (directly down) 
+
+    // Use this for initialization
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = true;
+        charaMesh = GameObject.Find("PlayerMesh");
         environmentLayerMask = LayerMask.GetMask("Environment");
+        player = transform.GetComponent<Player>();
+        //am = GetComponent<AnimeManager>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        runSpeed = 1.6f * moveSpeed;
     }
     // Update is called once per frame for physics
-    void FixedUpdate()
+    private void Update()
     {
-        // Check if the user is attempting to jump
-        float jumpAxis = Input.GetAxisRaw("Jump");
-        if (jumpAxis != 0 && rb.velocity.y <= 0)
+        //获取输入
+        if (!player.stay)
+            moveAxis = Input.GetAxis("Horizontal");
+        else
+            moveAxis = 0;
+
+        if (moveAxis > 0f)                              //面向判定&改向
         {
-            // Raycast from the feet of the player directly down (or the origin, doesn't matter)
-            bool hit = Physics.Raycast(rb.position - new Vector3(0f, 0f,0.5f), Vector3.down, 0.2f, environmentLayerMask);
-            // If the raycast hit something
-            if (hit)
-            {
-                Jump(jumpAxis * jumpForce * Time.deltaTime);
-            }
+            charaMesh.transform.rotation = frontRotation;
+            facingRight = true;
+        }
+
+        if (moveAxis < 0f)                              //面向判定&改向
+        {
+            charaMesh.transform.rotation = backRotation;
+            facingRight = false;
         }
     }
+
+    private void LateUpdate()
+    {
+        if (grounded)
+        {
+            this.transform.Translate(Vector3.right * Time.deltaTime * moveSpeed * moveAxis); //角色移动实现
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Check if the user is grounded
+        if (rb.velocity.z <= 0)
+        {
+            bool hit = Physics.Raycast(rb.position - new Vector3(0f, 0f, 0.5f), Vector3.down, 0.2f, environmentLayerMask);
+            if (hit)
+            {
+                grounded = true;
+            }
+        }
+        else
+        {
+            grounded = false;
+        }
+
+        float jumpAxis = Input.GetAxisRaw("Jump");
+        if (jumpAxis != 0 && grounded)
+        {
+            Jump(jumpAxis * jumpForce * Time.deltaTime);
+        }
+    }
+
     void Jump(float force)
     {
         if (force < 0)
@@ -45,14 +99,5 @@ public class Controller : MonoBehaviour {
         }
         rb.AddForce(new Vector3(0, 0, force));
     }
-
-    // Use this for initialization
-    void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
